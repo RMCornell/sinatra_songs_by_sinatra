f3bd753ac94c32f517afc6d6c59950bb60d1d7a7
kurohyou108 <robert.cornell78@gmail.com>
2015-05-09 18:04:31 -0600

    Created views directory and separated out erb files

--- main.rb
+++ main.rb
@@ -14,37 +14,8 @@ get '/contact' do
 end
 
 
-__END__
-@@layout
-<% title = "Songs by Sinatra" %>
-<!doctype html>
-<html lang="en">
-<head>
-  <title><%= title %></title>
-  <meta charset="utf-8">
-</head>
 
-<body>
-  <header>
-    <h1><%= title %></h1>
-    <nav>
-      <ul>
-        <li><a href="/" title="Home">Home</a></li>
-        <li><a href="/about" title="About">About</a></li>
-        <li><a href="/contact" title="Contact"">Contact</a></li>
-      </ul>
-    </nav>
-  </header>
-  <section>
-    <%= yield %>
-  </section>
-</body>
-</html>
-@@home
-<p>Welcome to this website about the songs of the great Frank Sinatra</p>
 
-@@about
-<p>This site is a demonstration of how to build a website using Sinatra.</p>
 
-@@contact
-<p>You can contact me by sending me an email at zen . alchemist at gmail.com</p>
+
+
