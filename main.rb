cf6bf272d2883dc4386c3119163a451e2ad0fa6f
kurohyou108 <robert.cornell78@gmail.com>
2015-05-11 19:51:49 -0600

    Completed Chapter 2 Jump Start Sinatra

--- main.rb
+++ main.rb
@@ -1,25 +1,23 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
+require 'slim'
+require 'sass'
+
+get ('/styles.css') {scss :styles }
 
 get '/' do
-  erb :home
+  slim :home
 end
 
 get '/about' do
-  erb :about
+  @title = "All About This Website"
+  slim :about
 end
 
 get '/contact' do
-  erb :contact
+  slim :contact
 end
 
 not_found do
-  erb :not_found
+  slim :not_found
 end
-
-
-
-
-
-
-
